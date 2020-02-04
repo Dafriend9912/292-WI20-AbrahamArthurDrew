e1dcfe19b5418465a63ee439a05e163c73c93beb
Drew Friend <dafriend@knox.edu>
2020-02-04 14:09:13 -0600

    Added script for results buttons

---
+++ CS292-Template/Assets/Scripts/ResultsButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultsButton : MonoBehaviour
+{
+    public GameObject playAgain;
+    public GameObject Exit;
+    public GameObject titleScreen;
+    public GameObject resultsScreen;
+    public TimerController timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        if (this.gameObject == Exit)
+        {
+            resultsScreen.SetActive(false);
+            titleScreen.SetActive(true);
+            timer.active = false;
+        }
+        else
+        {
+            resultsScreen.SetActive(false);
+            timer.active = true;
+        }
+    }
+}
